eba13da48a91237fa597fc83f4eb1b6576ca87d6
Machina maiki <machina@localhost.localdomain>
2022-01-17 11:29:47 +0300

    sum totals

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -57,23 +57,25 @@
                             </div>
                         </div>
                     </div>
-                    <div class="col-6 col-lg-3 col-md-6">
-                        <div class="card">
-                            <div class="card-body px-3 py-4-5">
-                                <div class="row">
-                                    <div class="col-md-4">
-                                        <div class="stats-icon green">
-                                            <i class="iconly-boldAdd-User"></i>
+                    @if (Auth::user()->role == 'Admin' || Auth::user()->role == 'Developer')
+                        <div class="col-6 col-lg-3 col-md-6">
+                            <div class="card">
+                                <div class="card-body px-3 py-4-5">
+                                    <div class="row">
+                                        <div class="col-md-4">
+                                            <div class="stats-icon green">
+                                                <i class="iconly-boldAdd-User"></i>
+                                            </div>
                                         </div>
-                                    </div>
-                                    <div class="col-md-8">
-                                        <h6 class="text-muted font-semibold">Total Overall</h6>
-                                        <h6 class="font-extrabold mb-0">{{ $totalAmount }}</h6>
+                                        <div class="col-md-8">
+                                            <h6 class="text-muted font-semibold">Total Overall</h6>
+                                            <h6 class="font-extrabold mb-0">{{ $totalAmount }}</h6>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
+                    @endif
                     {{-- <div class="col-6 col-lg-3 col-md-6">
                         <div class="card">
                             <div class="card-body px-3 py-4-5">
