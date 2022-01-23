2ecbc62862e1fd6ea6d96cbe97be9af6b821bb9b
Machina maiki <machina@localhost.localdomain>
2022-01-23 16:44:43 +0300

    wrong totals correction

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -26,7 +26,7 @@ class AdminController extends Controller
                 DB::raw("(sum(TransAmount)) as TransAmount"),
                 DB::raw("(DATE_FORMAT(TransTime, '%d-%M-%Y')) as TransTime")
                 )->where('BusinessShortCode', '7296354')->groupBy(DB::raw("DATE_FORMAT(TransTime, '%d-%M-%Y')"))->get();
-            $totalAmount = Players::where('BusinessShortCode', '7296354')->get()->sum('TransAmount');
+            // $totalAmount = Players::where('BusinessShortCode', '7296354')->get()->sum('TransAmount');
             $totalToday = Players::where('BusinessShortCode', '7296354')->whereDate('TransTime', date('Y-m-d'))->sum('TransAmount');
             return view('admin.dashboard', ['players' => $players, 'totalAmount'=>$totalAmount, 'totalToday'=>$totalToday]); 
         }
@@ -50,7 +50,7 @@ class AdminController extends Controller
                 DB::raw("(DATE_FORMAT(TransTime, '%d-%M-%Y')) as TransTime")
                 )->groupBy(DB::raw("DATE_FORMAT(TransTime, '%d-%M-%Y')"))->where('BusinessShortCode', $shortcode)->get();
             $totalToday = Players::whereDate('TransTime', date('Y-m-d'))->where('BusinessShortCode', $shortcode)->sum('TransAmount');
-            $totalAmount = Players::where('BusinessShortCode', $shortcode)->sum('TransAmount');
+            // $totalAmount = Players::where('BusinessShortCode', $shortcode)->sum('TransAmount');
             return view('admin.dashboard', ['players' => $players, 'totalAmount'=>$totalAmount, 'totalToday'=>$totalToday]);
         }
     }
