c94f92007557e802c2467aa5df21cdb4e81eba1c
Machina maiki <machina@localhost.localdomain>
2022-01-24 21:34:14 +0300

    db limit

--- app/Http/Livewire/PlayersTable.php
+++ app/Http/Livewire/PlayersTable.php
@@ -21,19 +21,19 @@ class PlayersTable extends Component
     {
         //if user is admin return all data
         if (Auth::user()->role == 'Jamii') {
-            $players = Players::whereDate('TransTime', date('Y-m-d'))->where('BusinessShortCode', '7296354')->orderBy('TransTime', 'DESC')->limit(50)->get();
+            $players = Players::whereDate('TransTime', date('Y-m-d'))->where('BusinessShortCode', '7296354')->orderBy('TransTime', 'DESC')->limit(20)->get();
             return view('livewire.players-table', ['players' => $players]);
         }
         //if user is admin return all data
         if (Auth::user()->role == 'Admin' || Auth::user()->role == 'Developer') {
-            $players = Players::where('BusinessShortCode', '!=', '7296354')->orderBy('TransTime', 'DESC')->limit(50)->get();
+            $players = Players::where('BusinessShortCode', '!=', '7296354')->orderBy('TransTime', 'DESC')->limit(20)->get();
             return view('livewire.players-table', ['players' => $players]);
         // if user is radio station, return specific data
         } else {
             $radio = Auth::user()->role;
             $shortcode=Radio::where('name', $radio)->first();
             $shortcode=$shortcode['shortcode'];
-            $players = Players::whereDate('TransTime', date('Y-m-d'))->where('BusinessShortCode', $shortcode)->orderBy('TransTime', 'DESC')->limit(50)->get();
+            $players = Players::whereDate('TransTime', date('Y-m-d'))->where('BusinessShortCode', $shortcode)->orderBy('TransTime', 'DESC')->limit(20)->get();
             return view('livewire.players-table', ['players' => $players]);
         }
     }
